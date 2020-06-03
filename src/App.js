6ee2e906678184cb026a6d56674bd3d2a7e1852a
wtlin1228 <wtlin1228@gmail.com>
2020-06-03 10:23:16 +0800

    test promise then setState

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    console.log("useEffect: call api");
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(true);
+      }, 0);
+    }).then((value) => {
+      console.log("useEffect: set state");
+      setShow(value);
+    });
+  }, []);
+
+  return <div className="App">{show && <h1>Hello World</h1>}</div>;
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import Enzyme, { mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import App from "./App";
+import { act } from "react-dom/test-utils";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new Adapter() });
+
+test("should render hello world", async () => {
+  console.log("test: start");
+
+  const wrapper = mount(<App />);
+
+  console.log(wrapper.debug());
+
+  await act(async () => {
+    await new Promise((resolve) => setTimeout(resolve, 0));
+    wrapper.update();
+  });
+
+  console.log(wrapper.debug());
+
+  console.log("test: end");
 });
